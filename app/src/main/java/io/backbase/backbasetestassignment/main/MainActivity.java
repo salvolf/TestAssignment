4260a4bd80465e7900fb6798e302b408376e1636
Salvatore La Fiura <salvatore@myorder.nl>
2018-04-17 16:28:33 +0200

    Added api key, integrated places api to select location
    Signed-off-by: Salvatore La Fiura <salvatore@myorder.nl>

--- app/src/main/java/io/backbase/backbasetestassignment/main/MainActivity.java
+++ app/src/main/java/io/backbase/backbasetestassignment/main/MainActivity.java
@@ -1,21 +1,34 @@
 package io.backbase.backbasetestassignment.main;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import io.backbase.backbasetestassignment.R;
 import io.backbase.backbasetestassignment.common.BaseFragmentActivity;
 import io.backbase.backbasetestassignment.databinding.ActivityMainBinding;
-import io.backbase.backbasetestassignment.main.fragments.CityFragment;
 import io.backbase.backbasetestassignment.main.fragments.HelpFragment;
 import io.backbase.backbasetestassignment.main.fragments.HomeFragment;
 
-public class MainActivity extends BaseFragmentActivity {
+public class MainActivity extends BaseFragmentActivity implements HomeFragment.HomeFragmentCallback {
+    private static final int PLACE_PICKER_REQUEST = 1010;
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+
     @NonNull private ActivityMainBinding binding;
     @NonNull private MainActivityViewModel viewModel;
+    @Nullable private Intent placeIntent;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,10 +37,9 @@ public class MainActivity extends BaseFragmentActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    showFragment(new HomeFragment(), false, binding.container.getId());
+                    showHomeFragment();
                     return true;
                 case R.id.navigation_dashboard:
-                    showFragment(new CityFragment(), false, binding.container.getId());
                     return true;
                 case R.id.navigation_notifications:
                     showFragment(new HelpFragment(), false, binding.container.getId());
@@ -45,8 +57,38 @@ public class MainActivity extends BaseFragmentActivity {
             viewModel = new MainActivityViewModel();
             binding.setViewModel(viewModel);
         }
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        binding.navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        showHomeFragment();
     }
 
+    private void showHomeFragment() {
+        HomeFragment homeFragment = new HomeFragment();
+        homeFragment.setCallback(this);
+        showFragment(homeFragment, false, binding.container.getId());
+
+    }
+
+    @Override
+    public void onAddNewPlaceClicked() {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+            Log.e(TAG, "GooglePlayServicesNotAvailableException");
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
--- app/src/main/java/io/backbase/backbasetestassignment/main/fragments/HomeFragment.java
+++ app/src/main/java/io/backbase/backbasetestassignment/main/fragments/HomeFragment.java
@@ -1,21 +1,41 @@
 package io.backbase.backbasetestassignment.main.fragments;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
-//    @NonNull HomeFragmentBinding binding;
+import io.backbase.backbasetestassignment.R;
+import io.backbase.backbasetestassignment.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    public interface HomeFragmentCallback {
+        void onAddNewPlaceClicked();
+    }
+
+    @NonNull FragmentHomeBinding binding;
+    @Nullable HomeFragmentCallback callback;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
-//        return binding.getRoot();
-        return container.getRootView();
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        binding.newplace.setOnClickListener(this);
+        return binding.getRoot();
+    }
 
+    public void setCallback(@Nullable HomeFragmentCallback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (callback != null) {
+            callback.onAddNewPlaceClicked();
+        }
     }
 }
